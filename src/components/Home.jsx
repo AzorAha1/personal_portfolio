7d56518abc759c1bb2066d7d52b13bf107285e4a
AzorAha1 <wizfaiz@icloud.com>
2026-01-03 12:43:47 +0100

    navbar done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@
 
 function Home() {
     return (
-        <section id='home' className='h-screen flex items-center justify-center'>
+        <section id='home' className='h-screen flex items-center justify-center from-black via-slate-950 to-black'>
             <h1 className='text-3xl text-amber-400'>Home</h1>
         </section>
     )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 function Navbar() {
     return (
-        <nav className='fixed left-0 top-0 bg-black/80 h-screen flex'>
+        <nav className='fixed left-20 top-10 bg-black/80 h-screen flex'>
             <ul className='flex flex-col gap-2 w-full'>
-                <li><a href="#home" className='text-amber-300 hover:text-white'>Home</a></li>
-                <li><a href="#about" className='text-amber-300 hover:text-white'>About</a></li>
-                <li><a href="#projects" className='text-amber-300 hover:text-white'>Projects</a></li>
-                <li><a href="#contact" className='text-amber-300 hover:text-white'>Contact</a></li>
+                <li><a href="#home" className='text-amber-300 hover:text-white border-r-4 border-transparent hover:border-amber-400'>Home</a></li>
+                <li><a href="#about" className='text-amber-300 hover:text-white border-r-4 border-transparent hover:border-amber-400'>About</a></li>
+                <li><a href="#projects" className='text-amber-300 hover:text-white border-r-4 border-transparent hover:border-amber-400'>Projects</a></li>
+                <li><a href="#contact" className='text-amber-300 hover:text-white border-r-4 border-transparent hover:border-amber-400'>Contact</a></li>
             </ul>
 
         </nav>
