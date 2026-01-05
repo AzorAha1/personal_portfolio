57be6fa127638f897b1cd4d165f3d167f29c098e
AzorAha1 <wizfaiz@icloud.com>
2026-01-05 20:25:09 +0100

    hero page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,16 @@ import wingx from '../assets/wingx.png';
 
 function Home() {
     return (
-        <section id='home' className='min-h-screen flex items-center justify-center px-8 pt-20'>
+        <section id='home' className='min-h-screen overflow-hidden bg-gradient-to-r from-black via-slate-950 relative flex items-center justify-center px-8 pt-20'>
+            <div className='w-72 h-72 absolute bottom-20 right-40 group hover:opacity-50 transition-all duration-1000 hover:rotate-5 hover:scale-150'>
+                <img 
+                    src={wingx} 
+                    alt="X-Wing"
+                    className='w-full h-full group-hover:opacity-100' 
+                />
+            </div>
+            
+
             <div className='space-y-8 text-center'>  {/* Increased spacing */}
                 <h1 className='text-4xl md:text-6xl font-bold text-amber-400 font-["StardosStencil"] tracking-wider'>
                     Mohammed Faisal Adamu
@@ -18,6 +27,8 @@ function Home() {
     )
 }
 
-
+{/* <div className='w-72 h-72 absolute bottom-20 right-40 opacity-20 group hover:opacity-50 
+               transition-all duration-700 
+               hover:scale-110 hover:rotate-2'> */}
 
 export default Home;
